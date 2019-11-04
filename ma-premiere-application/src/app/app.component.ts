dddf681b9bb9abc4aac16b90a266fe4713d46be7
LABBE Julien <jlabbe@alteca.fr>
2019-11-04 15:44:13 +0100

    Bouton pour afficher ou cacher image

--- ma-premiere-application/src/app/app.component.ts
+++ ma-premiere-application/src/app/app.component.ts
@@ -8,6 +8,25 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Gestion des wookies';
   title2 = 'Liste des wookies';
+
+  // Image
   imageUrl = 'https://oddstuffmagazine.com/wp-content/uploads/2012/09/0LsHp.jpg';
-  isDisplayLogo = false;
+  isDisplayLogo = true;
+
+  // Bouton
+  libelleSupprimerImage = 'Supprimer image';
+  libelleAfficherImage = 'Afficher image';
+
+  /**
+   * Fonction du bouton
+   */
+  fonctionClick() {
+    console.log("Bouton click");
+
+    this.isDisplayLogo = !this.isDisplayLogo;
+  }
+
+  get afficherImageOuiNon() : string {
+    return this.isDisplayLogo ? this.libelleSupprimerImage : this.libelleAfficherImage;
+  }
 }
