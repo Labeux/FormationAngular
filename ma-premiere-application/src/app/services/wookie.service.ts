b6b0d315928a74f8420c6fb18bf77b87ccf88fef
LABBE Julien <jlabbe@alteca.fr>
2019-11-05 17:26:58 +0100

    Correction

--- ma-premiere-application/src/app/services/wookie.service.ts
+++ ma-premiere-application/src/app/services/wookie.service.ts
@@ -6,16 +6,15 @@ import { Wookie } from '../models/wookie';
 })
 export class WookieService {
 
-  wookieList : Wookie[] = [];
-
   constructor() { }
 
   getAll() : Wookie[] {
+    const wookieList : Wookie[] = [];
 
-    this.wookieList.push(new Wookie(1, 'Wookie 1', new Date('01/01/2000')));
-    this.wookieList.push(new Wookie(2, 'Wookie 2', new Date()));
-    this.wookieList.push(new Wookie(3, 'Wookie 3', new Date()));
+    wookieList.push(new Wookie(1, 'Wookie 1', new Date('01/01/2000')));
+    wookieList.push(new Wookie(2, 'Wookie 2', new Date()));
+    wookieList.push(new Wookie(3, 'Wookie 3', new Date()));
 
-    return this.wookieList;
+    return wookieList;
   }
 }
