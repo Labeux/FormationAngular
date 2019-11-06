dfdf768645d24df8122d9f395c8d5108c74dcb96
LABBE Julien <jlabbe@alteca.fr>
2019-11-06 17:04:26 +0100

    Gestion de la mémoire

--- ma-premiere-application/src/app/liste-des-wookies/liste-des-wookies.component.ts
+++ ma-premiere-application/src/app/liste-des-wookies/liste-des-wookies.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit, ComponentFactory, ComponentFactoryResolver } from '@angular/core';
+import { Component, OnInit, ComponentFactory, ComponentFactoryResolver, OnDestroy } from '@angular/core';
 import { Wookie } from '../models/wookie';
 import { LoggerService } from '../services/logger.service';
 import { WookieService } from '../services/wookie.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-liste-des-wookies',
   templateUrl: './liste-des-wookies.component.html',
   styleUrls: ['./liste-des-wookies.component.css']
 })
-export class ListeDesWookiesComponent implements OnInit {
+export class ListeDesWookiesComponent implements OnInit, OnDestroy {
   title = 'Gestion des wookies';
   title2 = 'Liste des wookies';
 
@@ -24,15 +25,22 @@ export class ListeDesWookiesComponent implements OnInit {
 
   currentWookie : Wookie;
 
+  private _subscriptions: Subscription[] = [];
+
   constructor(private _logger: LoggerService, 
               private _wookieService: WookieService
               ) { 
     
   }
-
+  
+  ngOnDestroy(): void {
+    this._subscriptions.forEach(souscription => souscription.unsubscribe());
+  }
+  
   ngOnInit() {
     //this.wookieList = _wookieService.getAll();
-    this._wookieService.getAll().subscribe(tableau => this.wookieList = tableau);
+    const laSubscription = this._wookieService.getAll().subscribe(tableau => this.wookieList = tableau);
+    this._subscriptions.push(laSubscription);
   }
 
   fonctionClickEdition(wookie : Wookie) {
